a4d0d1f2c87db28accea5a79374adaa9f1eed8a7
Marion802 <marion.sermande@gmail.com>
2025-02-08 21:04:57 +0100

    home1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   template: `
-    <app-header></app-header>
-    <h1>Welcome to {{title}}!</h1>
-    <app-footer></app-footer>
+   
 
     <router-outlet />
   `,
