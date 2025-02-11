e7954a054a982f2ba330fcc338592aa68e6e587b
Marion802 <marion.sermande@gmail.com>
2025-02-11 23:57:16 +0100

    designandform

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArtisanListComponent } from './artisan-list/artisan-list.component';
 import { ArtisanDetailComponent } from './artisan-detail/artisan-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path : 'home', component: HomeComponent},
     { path: 'artisans', component: ArtisanListComponent },
-    { path: 'artisan/:id', component: ArtisanDetailComponent }
+    { path: 'artisan/:id', component: ArtisanDetailComponent },
+    { path: '**', component: NotFoundComponent } // Route pour la page 404
 ];
--- src/app/artisan-detail/artisan-detail.component.ts
+++ src/app/artisan-detail/artisan-detail.component.ts
@@ -4,16 +4,22 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtisanService } from '../artisan.service';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-artisan-detail',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './artisan-detail.component.html',
   styleUrls: ['./artisan-detail.component.scss']
 })
 export class ArtisanDetailComponent implements OnInit {
   artisan: any;
+  contactForm = {
+    name: '',
+    subject: '',
+    message: ''
+  };
 
   constructor(private route: ActivatedRoute, private artisanService: ArtisanService) { }
 
@@ -26,6 +32,13 @@ export class ArtisanDetailComponent implements OnInit {
     } else {
       // Gérer le cas où l'ID est null
       console.error('ID de l\'artisan non trouvé');
+    }
+  }
+
+  sendEmail(): void {
+    if (this.artisan && this.artisan.email) {
+      const mailtoLink = `mailto:${this.artisan.email}?subject=${encodeURIComponent(this.contactForm.subject)}&body=${encodeURIComponent(this.contactForm.message)}`;
+      window.location.href = mailtoLink;
     }
   }
 }
